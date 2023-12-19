184cd8c259ba2b2bcc91ea6ff80608805ec19152
Yaroslav <yibelyaev@icloud.com>
2023-12-19 18:57:56 +0300

    Add: Header and styles

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import s from './Header.module.scss';
+
+const Header = () => {
+  return (
+    <header className={s.header}>
+      <h1 className={s.title}>todos</h1>
+    </header>
+  );
+};
+
+export default Header;
