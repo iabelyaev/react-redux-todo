cb0367d06902179d5e3acdf170bac7fc08d92623
Yaroslav <yibelyaev@icloud.com>
2023-12-19 20:23:36 +0300

    fix warning

--- src/components/TodoItem/TodoItem.jsx
+++ src/components/TodoItem/TodoItem.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { string, shape, number, bool } from 'prop-types';
 import cx from 'classnames';
